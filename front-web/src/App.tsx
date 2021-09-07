d982cba58a706382d02698e30c63a4d3e3a2ffcb
Manoel <manoelll@gmail.com>
2021-09-06 21:18:18 -0300

    Graficos

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import Filter from 'components/filter';
+import SalesByDate from 'components/sales-by-date';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <Header/>
+  <div className="app-container">
+    <Filter/>
+    <SalesByDate/>
+  </div>
+  </>
+   
+  
+    );
 }
 
 export default App;
